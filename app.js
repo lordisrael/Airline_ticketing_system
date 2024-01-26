6264f699505b8572bb3edb951264eb3d8f2b5600
lordisrael <josephisrael206@gmail.com>
2024-01-26 22:04:22 +0100

    added sequelize

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authRoute = require("./routes/authRoutes");
+const adminRoute = require("./routes/adminRoutes")
 const dbConnect = require("./db/database");
+const { sequelize, User, Airport } = require("./model/index");
 
 app.get("/", (req, res) => {
   res.send(
@@ -16,6 +18,17 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use("/api", authRoute);
+app.use("/api/admin", adminRoute)
+
+sequelize
+  .sync() // Use { force: true } for development, be cautious in production
+  .then(() => {
+    console.log("Database synchronized successfully!");
+    // Start your express app here
+  })
+  .catch((error) => {
+    console.error("Unable to synchronize database: ", error);
+  });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
