59e239a268b5e28bcd88ee62b242fd579728698c
lordisrael <josephisrael206@gmail.com>
2024-01-27 23:23:17 +0100

    added table schema for price

---
+++ model/price.js
@@ -0,0 +1,46 @@
+// models/Price.js
+const { DataTypes } = require("sequelize");
+const Sequelize = require("../db/database");
+const Flight = require("./flight"); // Import the Flight model
+
+const Price = Sequelize.define(
+  "Price",
+  {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    flightId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: Flight,
+        key: "id",
+        onDelete: "CASCADE", // If the associated Flight record is deleted, also delete the Price record
+      },
+    },
+    economy: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    business: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    firstClass: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    // Additional attributes related to pricing
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Define the association
+Price.belongsTo(Flight, { foreignKey: "flightId" });
+
+module.exports = Price;
