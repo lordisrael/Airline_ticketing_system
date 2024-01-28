1e91f02aad94cb399fa7516ffd767b586ede5b64
lordisrael <josephisrael206@gmail.com>
2024-01-28 20:24:27 +0100

    added middleware to handle error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,12 @@
 const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("../error/custom-api");
+const {
+  ConflictError,
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+} = require("../error");
+
 
 const errorMiddleware = (err, req, res, next) => {
   let customError = {
@@ -9,24 +17,20 @@ const errorMiddleware = (err, req, res, next) => {
   // if(err instanceof CustomAPIError) {
   //     return res.status(err.statusCode).json({msg:err.message})
   // }
-  // if(err.name === 'SequelizeUniqueConstraintError') {
-  //     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
-  //     customError.statusCode = 400
-  // }
-  // if (err.name === 'SequelizeDatabaseError' && err.parent && err.parent.code === '23503') {
-  //     // This handles foreign key constraint violations.
-  //     customError.msg = 'Referenced record not found.';
-  //     customError.statusCode = 400;
-  //   }
-  if (err.name === "SequelizeValidationError") {
-    customError.msg = err.errors.map((error) => error.message).join(",");
-    customError.statusCode = 400;
+  if (err instanceof UnauthenticatedError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: err.message });
   }
-  // if (err.name === 'SequelizeDatabaseError' && err.parent && err.parent.code === '23505') {
-  //     // This handles unique constraint violations.
-  //     customError.msg = 'Duplicate entry.';
-  //     customError.statusCode = 400;
-  // }
+
+  if (err.name === "SequelizeUniqueConstraintError") {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: "Duplicate entry. Please provide unique data.",
+    });
+  }
+  if (err.name === 'SequelizeDatabaseError' && err.parent && err.parent.code === '23503') {
+      // This handles foreign key constraint violations.
+      customError.msg = 'Referenced record not found.';
+      customError.statusCode = 400;
+    }
   return res.status(customError.statusCode).json({
     msg: customError.msg,
     stack: customError.stack,
@@ -34,3 +38,5 @@ const errorMiddleware = (err, req, res, next) => {
 };
 
 module.exports = errorMiddleware;
+
+
