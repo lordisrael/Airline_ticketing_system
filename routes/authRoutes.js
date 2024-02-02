47199d81bfc52f9f83cb69939f3f0f6b2fab84fc
lordisrael <josephisrael206@gmail.com>
2024-02-02 22:10:02 +0100

    Added middleware for profile route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
-    createUser
+    createUser, login, profile, logout
 } = require("../controllers/authCtrl");
 
 router.post("/register", createUser);
-
+router.post("/login", login)
+router.get("/logout", logout)
+router.get("/profile", authMiddleware, profile);
 
 module.exports = router;
