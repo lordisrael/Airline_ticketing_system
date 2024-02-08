6f8607b986a4897a1c37e28a65e0b2c784e5156d
lordisrael <josephisrael206@gmail.com>
2024-02-08 14:13:05 +0100

    Added route for get all airport

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
   searchFlight, searchArrivingAirportOnFlight, searchDepartureDateOnFlight
 } = require("../controllers/flightCtrl");
+const {
+  getAllAirport
+} = require("../controllers/airportCtrl")
 
 router.get("/results", searchFlight)
 router.get("/arriving-airport/:id", searchArrivingAirportOnFlight)
 router.get("/date", searchDepartureDateOnFlight)
+router.get("/get-all-airport", getAllAirport);
 
 module.exports = router
