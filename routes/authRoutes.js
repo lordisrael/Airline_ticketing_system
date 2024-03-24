fb2ae2f0e8be6c92742a6744d7e5d94032ca6616
lordisrael <josephisrael206@gmail.com>
2024-03-24 10:47:14 +0100

    Installed swagger-ui-express yamljs cors...

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
 const {
-    createUser, login, profile, logout
+    createUser, login, profile, logout, updatePassword
 } = require("../controllers/authCtrl");
 
 router.post("/register", createUser);
 router.post("/login", login)
 router.get("/logout", logout)
+router.post("/update-password",authMiddleware, updatePassword)
 router.get("/profile", authMiddleware, profile);
 
 module.exports = router;
+
