118e9fbd43daea82b8d26883bc81ebc06d0d9a31
lordisrael <josephisrael206@gmail.com>
2024-03-29 00:36:23 +0100

    Move db to cloud

--- db/database.js
+++ db/database.js
@@ -3,16 +3,28 @@ const dotenv = require("dotenv"); // Import dotenv
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    port: process.env.POSTGRES_PORT || 5432, 
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.POSTGRES_DB,
+//   process.env.POSTGRES_USER,
+//   process.env.POSTGRES_PASSWORD,
+//   {
+//     host: process.env.POSTGRES_HOST,
+//     dialect: "postgres",
+//     port: process.env.POSTGRES_PORT || 5432, 
+//   }
+// );
+
+const sequelize = new Sequelize(process.env.POSTGRES_HOST, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 
 sequelize
   .authenticate()
